1da32fefb67c4c3a8c197d4ddc28ca30182aaef3
alhantawi <alhantawii@gmail.com>
2024-05-18 05:17:10 +0200

    HERO

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-  <h1>Hello</h1>
-  </div>
+    <main className=" relative bg-black-100">
+     <Hero />
     </main>
   );
 }
