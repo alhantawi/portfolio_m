97165a4a9cccd3437d1f0b9404b8a8d5613631d4
alhantawi <alhantawii@gmail.com>
2024-05-22 04:51:41 +0200

    Mrquee

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -87,10 +87,10 @@ export const BentoGridItem = ({
          <div
           className={cn(
             titleClassName,
-            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 justify-center flex flex-col px-5 p-5 lg:p-10"
           )}
         >
-         <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
+         <div className="font-sans font-extralight md:max-w-96 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
             {description}
 
             <div
@@ -99,7 +99,7 @@ export const BentoGridItem = ({
             {title}
           </div>
           {id === 2 && <GlobeDemo />}
-          {id === 5 && <Stack />}
+          {id === 3 && <Stack />}
           </div> 
         </div>
 
--- components/ui/Stack.tsx
+++ components/ui/Stack.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
-// import Marquee from 'react-fast-marquee'
-
-
+import React from 'react';
+import Image from "next/image"; 
 import img3 from "../../public/insta.svg"
+import img1 from "../../public/gsap.svg"
+import Marquee from 'react-fast-marquee';
 
 
 const Stack = () => {
   return (
     <div>
-       <div> 
-    
-        <img src={img3} alt="" />
-        <img src={img3} alt="" />   
-        <img src={img3} alt="" />
-    
-      </div>
+      <div className='w-full flex justify-between'> 
+             <Marquee>  
+                 <Image src={img3}  
+                    alt="GFG logo imported from public directory" /> 
+                    <Image src={img1} alt="GFG logo imported from public directory" /> 
+                    </Marquee> 
+            </div> 
     </div>
-  )
-}
+  );
+};
 
-export default Stack
+export default Stack;
