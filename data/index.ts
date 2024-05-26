5a16d633f0f4715ebbe452ae8986a448b77e9956
alhantawi <82392782+alhantawi@users.noreply.github.com>
2024-05-27 01:59:51 +0200

    index

--- data/index.ts
+++ data/index.ts
@@ -50,7 +50,7 @@ export const navItems = [
     {
       id: 5,
       title: "Currently building a JS Animation library",
-      description: "The Inside Scoop",
+      description: "The Inside Scoop with three js und GSAP",
       className: "md:col-span-3 md:row-span-2",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
       titleClassName: "justify-center md:justify-start lg:justify-center",
